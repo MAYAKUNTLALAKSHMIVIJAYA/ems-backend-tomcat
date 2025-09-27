54a2abfb2774b00dca44f85f9c9d74c1d79cf48b
MAYAKUNTLALAKSHMIVIJAYA <mayakuntlalakshmi216@gmail.com>
2025-09-27 07:48:40 +0530

    Update EmployeeController.java

--- src/main/java/com/ems/controller/EmployeeController.java
+++ src/main/java/com/ems/controller/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import java.util.Random;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:9090/ems-frontend/")
 @RequestMapping("/api/employees")
 public class EmployeeController {
 
